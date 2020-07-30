23df0067d27ba4fcd540ca2fa448069734de5bc8
Kacper Barański <kacper.baranski@hotmail.com>
2020-07-31 00:19:15 +0200

    FIRST CHANGE

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Check(int valueCheck)
+        {
+            var value = "";
+            if (valueCheck % 3 == 0) value = "Fizz";
+            else value = valueCheck.ToString();
+            if (valueCheck % 5 == 0) value = "Buzz";
+            if (valueCheck % 3 == 0 && valueCheck % 5 == 0) value = "FizzBuzz";
+            return value;
+        }
+
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+                Console.WriteLine("Podal Liczbe");
+                var CheckValue = new FizzBuzz();
+                var value = Liczba();               
+                Console.WriteLine(CheckValue.Check(value));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+           
+        }
+
+        private static int Liczba()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int value)) throw new Exception("Podana Wartosć jest nie prawidłowa!");
+            return value;
+        }
+    }
+}
